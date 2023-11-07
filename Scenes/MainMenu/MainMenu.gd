3d825e0d65cbda7b990aa4a7f12f5e40f2d70056
OuterGuy <sebastianfilloy@gmail.com>
2023-11-07 16:06:52 -0300

    Main Menu implemented

---
+++ Scenes/MainMenu/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Scenes/OptionsMenu/OptionsMenu.tscn")
+
+func _on_quit_game_pressed():
+	get_tree().quit()
