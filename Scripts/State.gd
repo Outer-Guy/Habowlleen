d6ab2191ca93ffbdeeff0aa50c80e9cbe4dfe77d
Outer-Guy <66810009+Outer-Guy@users.noreply.github.com>
2023-11-25 04:20:25 -0300

    StateMachine And Camera Start

---
+++ Scripts/State.gd
@@ -0,0 +1,21 @@
+extends Node
+class_name State
+
+signal Transitioned
+
+func _ready():
+	pass
+
+func Enter():
+	
+	pass
+	
+func Exit():
+	pass
+	
+func Update(delta: float):
+	
+	pass
+	
+func Physics_Update(delta: float):
+	pass
