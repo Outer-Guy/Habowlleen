401c5e0eb262f5182c1d7c15788fe629229f86bd
Outer-Guy <66810009+Outer-Guy@users.noreply.github.com>
2023-11-26 21:36:55 -0300

    Camera Repaired

---
+++ Scenes/tombstone.gd
@@ -0,0 +1,22 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	
+	#if body.name == "Head"
+	pass # Replace with function body.
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print_debug(body.name)
+	pass # Replace with function body.
